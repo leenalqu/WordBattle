fd0de75cfb4472cce070db52ec9c279fbb5301a6
Jiaxi Huang <huangjx1025@gmail.com>
2025-03-25 22:09:30 +0000

    Added code for Current_Word. Bug: Background_Colour displays black instead of white

--- UI_v1.py
+++ UI_v1.py
@@ -13,9 +13,27 @@ BUTTON_COLOR = (200, 200, 200)
 TEXT_COLOR = (0, 0, 0)
 FONT_SIZE = 36
 
+#Config_Current_Word
+CURRENT_WORD_TITLE = "CURRENT WORD"
+CURRENT_WORD_TITLE_POS = (SCREEN_WIDTH // 2, 85)
+CURRENT_WORD_BOX_WIDTH = 81.9
+CURRENT_WORD_BOX_HEIGHT = 114.4
+CURRENT_WORD_BOX_SPACING = 100
+CURRENT_WORD_BOX_START_X = 210
+CURRENT_WORD_BOX_Y = 120
+CURRENT_WORD_BOXES = 4
+
 #Config_Screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Letter Card Game")
+
+#Config_Font
+font = pygame.font.Font(None, FONT_SIZE)
+
+def draw_current_word():
+    #Draw Title
+    text = font.render(CURRENT_WORD_TITLE, True, TEXT_COLOR)
+    screen.blit(text, (CURRENT_WORD_TITLE_POS[0] - text.get_width() // 2, CURRENT_WORD_TITLE_POS[1]))
 
 #Main Loop
 running = True
@@ -27,6 +45,9 @@ while running:
     #Draw Background
     screen.fill(BACKGROUND_COLOR)
 
-#QuitS
+    # Draw sections
+    draw_current_word()
+
+#Quit
 pygame.quit()
 sys.exit()
