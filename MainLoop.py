15b429aee4b99ed4b27f512541ecbfc4b8ee84e4
leeno <Leenoalqurashi@gmail.com>
2025-05-10 13:52:43 +0100

    Found many bugs. Fixed some issues i ahd in my nested loops. found out that at one point penalty cards and bot's turn were in an endless loop. Fully integrated the star card to my loop so now the player and bot can use it to change a word. The timer now works.

--- MainLoop.py
+++ MainLoop.py
@@ -71,6 +71,8 @@ class Player:
         # Make sure the letter exists.
         if letter in self.cards:
             self.cards.remove(letter)
+        elif "*" in self.cards:
+            self.cards.remove("*")
 
     # Checks if the player has used all of their cards.
     def won_game(self):
@@ -163,60 +165,82 @@ while True:
         # Check if the player answered within 15 seconds.
         if time_taken <= 15:
 
-            # Check if the new word is valid.
-            if game.check_exists(new_word):
+            # Check if the new word is valid and differs by 1 letter.
+            if game.check_exists(new_word) and game.is_one_letter_dif(
+                    current_word, new_word
+            ):
+                changed = False
 
-                # Check that the word differs by only 1 letter.
-                if game.is_one_letter_dif(current_word, new_word):
+                # Go through the letters.
+                for i in range(3) :
 
-                    # Go through the letters.
-                    for i in range(0, 3) :
+                    # Find the changed letter.
+                    if current_word[i] != new_word[i]:
+                        changed_letter = new_word[i]
 
-                        # Find the changed letter.
-                        if current_word[i] != new_word[i]:
-                            changed_letter = new_word[i]
+                        # Check if the player has the letter in their stack.
+                        if changed_letter in player1.cards or "*" in player1.cards:
+                            player1.remove_cards(changed_letter)
+                            game.quicksort(player1.cards)
+                            used_cards.append(changed_letter)
 
-                            # Check if the player has the letter in their stack.
-                            if changed_letter in player1.cards:
-                                player1.remove_cards(changed_letter)
-                                game.quicksort(player1.cards)
-                                used_cards.append(changed_letter)
+                            # Update the current word.
+                            current_word = new_word
 
-                                # Update the current word.
-                                current_word = new_word
+                            # Switch turns.
+                            current_player = player2
+                            print("It is now the bot's turn.")
 
-                                # Add it to used words.
-                                player1.used_words.add(current_word)
-                                current_player = player2
-                            else:
-                                if not deck:
-                                    deck = game.fisher_shuffle(used_cards.copy())
-                                    used_cards = []
+                            # Add it to used words.
+                            player1.used_words.add(current_word)
+                            changed = True
+                            break
+                if not changed:
+                    if not deck:
+                        deck = game.fisher_shuffle(used_cards.copy())
+                        used_cards = []
 
-                                # Give the player a penalty.
-                                player1.add_card(deck.pop())
-                                game.quicksort(player1.cards)
+                    # Give the player a penalty.
+                    player1.add_card(deck.pop())
+                    print(f"{player1.name} got a penalty card.")
+                    game.quicksort(player1.cards)
 
-                                # Switch turns.
-                                current_player = player2
+                    # Switch turns.
+                    current_player = player2
+
+            else:
+
+                # The player entered an invalid word.
+                if not deck:
+
+                    # Shuffle used cards and put them back into the deck.
+                    deck = game.fisher_shuffle(used_cards.copy())
+                    used_cards = []
+                player1.add_card(deck.pop())
+                print(f"{player1.name} entered an invalid word.")
+                game.quicksort(player1.cards)
+
+                # Switch turns.
+                current_player = player2
         else:
+
+            # The player took too long.
             if not deck:
 
                 # Shuffle used cards and put them back into the deck.
                 deck = game.fisher_shuffle(used_cards.copy())
                 used_cards = []
-
-            # Give a penalty card for taking too long.
             player1.add_card(deck.pop())
+            print(f"{player1.name} got a penalty card for taking too long.")
             game.quicksort(player1.cards)
-
-            # Switch turns.
             current_player = player2
+
 
     # Bot's turn.
     elif current_player == player2:
-        print(f"Your current cards: {player2.cards}")
+        print(f"Bot's current cards: {player2.cards}")
         print(f"It's the bot's turn. The word is: {current_word}")
+        # inside if loop
 
         # Timer for the bot.
         current_timer = 0
@@ -236,44 +260,64 @@ while True:
             # Check again for bot's move.
             bot_word = player2.play_turn(current_word, current_timer)
 
+        # Handles bot_word when it's a tuple.
+        if isinstance(bot_word, tuple):
+            bot_answer, bot_used_card = bot_word
+        else:
+            bot_answer = bot_word
+
+        if not bot_answer:
+            if not deck:
+                deck = game.fisher_shuffle(used_cards.copy())
+                used_cards = []
+            player2.add_card(deck.pop())
+            print("The bot failed to change the word.")
+            game.quicksort(player2.cards)
+            current_player = player1
+            player2.end_turn()
+            continue
+
         # Wait a bit to make it feel more natural.
         time.sleep(0.5)
-        print(f"The bot changed the word to: {bot_word}")
-        print(f"New current word is: {current_word}")
+        changed = False
 
-        # Check if the word is valid.
-        if game.check_exists(bot_word):
-
-            # Check that the word differs by only 1 letter.
-            if game.is_one_letter_dif(current_word, bot_word):
+        # Check if the word is valid and differs by only 1 letter.
+        if bot_answer and game.check_exists(bot_answer) and game.is_one_letter_dif(
+                current_word, bot_answer
+        ):
 
                 # Go through the letters.
-                for i in range(3):
-                    if current_word[i] != bot_word[i]:
-                        changed_letter = bot_word[i]
+            for i in range(3):
+                if current_word[i] != bot_answer[i]:
+                    changed_letter = bot_answer[i]
 
-                        # Check if the bot has the letter in its stack.
-                        if changed_letter in player2.cards:
-                            player2.remove_cards(changed_letter)
-                            game.quicksort(player2.cards)
-                            used_cards.append(changed_letter)
+                    # Check if the bot has the letter in its stack.
+                    if changed_letter in player2.cards or "*" in player2.cards:
+                        player2.remove_cards(changed_letter)
+                        game.quicksort(player2.cards)
+                        used_cards.append(changed_letter)
 
-                            # Update current word.
-                            current_word = bot_word
-                            current_player = player1
-                            player2.end_turn()
-                        else:
-                            if not deck:
-                                deck = game.fisher_shuffle(used_cards.copy())
-                                used_cards = []
+                        # Update current word.
+                        current_word = bot_answer
+                        print(f"The bot changed the word to {bot_answer} ")
+                        print(f"The current word is: {current_word}")
+                        current_player = player1
+                        player2.end_turn()
+                        changed = True
+                        break
+            if not changed:
+                if not deck:
+                    deck = game.fisher_shuffle(used_cards.copy())
+                    used_cards = []
 
-                            # Give the bot a penalty card.
-                            player2.add_card(deck.pop())
-                            game.quicksort(player2.cards)
+                # Give the bot a penalty card.
+                player2.add_card(deck.pop())
+                print("The bot got a penalty card")
+                game.quicksort(player2.cards)
 
-                            # Switch turns.
-                            current_player = player1
-                            player2.end_turn()
+                # Switch turns.
+                current_player = player1
+                player2.end_turn()
         else:
             if not deck:
                 deck = game.fisher_shuffle(used_cards.copy())
@@ -281,21 +325,9 @@ while True:
 
             # Bot gets a penalty if the word is invalid.
             player2.add_card(deck.pop())
+            print("Bot got a penalty card.")
             game.quicksort(player2.cards)
 
             # Switch turns.
             current_player = player1
             player2.end_turn()
-
-# Testing my code.
-if __name__ == "__main__":
-    L_player = Player("Leen", ["A, B, C, D"])
-
-    # Test add_card.
-    L_player.add_card("H")
-
-    # Test remove_card.
-    L_player.remove_cards("A")
-
-    # Test won_game; I'm expecting a 'False'.
-    L_player.won_game()
